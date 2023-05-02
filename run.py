9f106ab8edde801917199e2ccc4503f71afef0cb
Lase <lase_star@mail.ru>
2023-05-02 17:32:41 +0300

    Add_dz_2

--- run.py
+++ run.py
@@ -3,9 +3,8 @@ from wsgiref.simple_server import make_server
 from lase_framework.main import Framework
 from urls import routes, fronts
 
-
 application = Framework(routes, fronts)
 
-with make_server('', 8040, application) as httpd:
-    print("Запуск на порту 8040...")
+with make_server('', 8060, application) as httpd:
+    print("Запуск на порту 8060...")
     httpd.serve_forever()
